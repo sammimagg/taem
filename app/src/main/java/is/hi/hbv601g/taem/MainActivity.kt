ac3864f47103beb76828dde28e9bdbf0e531e846
sammimagg <sammimagg@gmail.com>
2023-02-06 22:22:39 +0000

    base

---
+++ app/src/main/java/is/hi/hbv601g/taem/MainActivity.kt
@@ -0,0 +1,11 @@
+package `is`.hi.hbv601g.taem
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
