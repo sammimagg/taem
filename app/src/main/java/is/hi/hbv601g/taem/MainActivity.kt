048892cc490af6a610c6cf2345011a816168db8d
sammimagg <sammimagg@gmail.com>
2023-02-28 22:32:06 +0000

    Bottom navigation & xml for login

--- app/src/main/java/is/hi/hbv601g/taem/MainActivity.kt
+++ app/src/main/java/is/hi/hbv601g/taem/MainActivity.kt
@@ -1,11 +1,48 @@
 package `is`.hi.hbv601g.taem
 
+import android.nfc.NdefMessage
+import android.nfc.NdefRecord
+import android.nfc.NdefRecord.createTextRecord
+import android.nfc.NfcAdapter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import `is`.hi.hbv601g.taem.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.clockinout -> replaceFragment(ClockInOutFragment())
+                R.id.schedule -> replaceFragment(ScheduleFragment())
+                R.id.time_and_attendance -> replaceFragment(TimeAndAttendaceFragment())
+                R.id.profile -> replaceFragment(ProfileFragment())
+                R.id.drivinglog -> replaceFragment(DrivingLogFragment())
+
+                else -> {
+
+                }
+            }
+            true
+        }
+
     }
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
+
+    }
+
 }
