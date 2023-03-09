1bd3d09c7bc1bd5d6ccb69e3c4949d0afb6c1544
dorijv <hjv6@hi.is>
2023-03-09 16:18:50 +0000

    DB
    * Uppskrift að DB
    * Viljum ábyggilega refactora yfir í sérklasa, því þetta er langt helvíti.
    * Ref: https://developer.android.com/training/data-storage/sqlite#kotlin

--- app/src/main/java/is/hi/hbv601g/taem/Networking/SessionUser.kt
+++ app/src/main/java/is/hi/hbv601g/taem/Networking/SessionUser.kt
@@ -2,5 +2,15 @@ package `is`.hi.hbv601g.taem.Networking
 
 data class SessionUser(
     var username: String,
-    var authToken: String
-)
+    var accessToken: String,
+
+
+    ) {
+    override fun toString(): String {
+        return "SessionUser(username='$username', authToken='$accessToken')"
+    }
+
+
+}
+
+
