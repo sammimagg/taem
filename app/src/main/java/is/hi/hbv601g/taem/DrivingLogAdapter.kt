97a0276babab90267ade621b8ccef3c911f1a44b
kbg15 <kbg15@hi.is>
2023-04-20 03:36:35 +0000

    Driving log keyrir, en er að skila null eins og er? kannski bara eh user issue.

--- app/src/main/java/is/hi/hbv601g/taem/DrivingLogAdapter.kt
+++ app/src/main/java/is/hi/hbv601g/taem/DrivingLogAdapter.kt
@@ -15,77 +15,42 @@ import `is`.hi.hbv601g.taem.Networking.Driving
 import `is`.hi.hbv601g.taem.R
 import `is`.hi.hbv601g.taem.Storage.db
 
-class DrivingLogAdapter(var drivingSessions: List<Driving>) :
-    RecyclerView.Adapter<DrivingLogAdapter.ViewHolder>() {
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val dateTextView: TextView = itemView.findViewById(R.id.dateTextView)
-        val startTimeTextView: TextView = itemView.findViewById(R.id.startTimeTextView)
-        val endTimeTextView: TextView = itemView.findViewById(R.id.endTimeTextView)
-        val distanceTextView: TextView = itemView.findViewById(R.id.distanceTextView)
+
+
+import java.time.format.DateTimeFormatter
+
+class DrivingLogAdapter(private var drivingSessions: List<Driving>) : RecyclerView.Adapter<DrivingLogAdapter.ViewHolder>() {
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val dateTextView: TextView = view.findViewById(R.id.dateTextView)
+        val distanceTextView: TextView = view.findViewById(R.id.distanceTextView)
+        val timeTextView: TextView = view.findViewById(R.id.timeTextView)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view =
-            LayoutInflater.from(parent.context).inflate(R.layout.driving_log_item, parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.driving_log_item, parent, false)
         return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val drivingSession = drivingSessions[position]
+
+        // Set the date, distance, and time
         holder.dateTextView.text = drivingSession.date
-        holder.startTimeTextView.text = drivingSession.startTime
-        holder.endTimeTextView.text = drivingSession.endTime
-        holder.distanceTextView.text = "${drivingSession.distance} km"
+        holder.distanceTextView.text = drivingSession.distance.toString()
+        holder.timeTextView.text = "${drivingSession.startTime} - ${drivingSession.endTime}"
     }
 
-    override fun getItemCount(): Int {
-        return drivingSessions.size
-    }
+    override fun getItemCount(): Int = drivingSessions.size
 
-    fun updateData(drivingSessions: List<Driving>) {
+    fun setData(drivingSessions: List<Driving>) {
         this.drivingSessions = drivingSessions
         notifyDataSetChanged()
     }
 
-companion object {
-    /** Hvað er þetta? */
-        fun fetchDrivingLog(ssn: String, onSuccess: (List<Driving>) -> Unit, onError: (String) -> Unit, context: Context) {
-            val queue = Volley.newRequestQueue(context)
-            var url = "https://www.hiv.is/api/driving/"
-
-            val db2 = db.SessionUserContract.DBHelper(context).readableDatabase
-            val cursor = db2.query(
-                `is`.hi.hbv601g.taem.Storage.db.SessionUserContract.SessionUserEntry.TABLE_NAME,   // The table to query
-                null,             // The array of columns to return (pass null to get all)
-                null,              // The columns for the WHERE clause
-                null,          // The values for the WHERE clause
-                null,                   // don't group the rows
-                null,                   // don't filter by row groups
-                BaseColumns._ID              // The sort order
-            )
-            with(cursor) {
-                moveToLast()
-            }
-            var ssnToUse = cursor.getString(4)
-            url += ssnToUse
-
-            println("Using URL: $url ********************")
-
-            val request = JsonObjectRequest(
-                Request.Method.PUT, url, null,
-                { response ->
-                    // Handle the JSON response
-                    val gson = Gson()
-                    val drivingSessions = gson.fromJson(response.toString(), Array<Driving>::class.java).toList()
-                    onSuccess(drivingSessions)
-                },
-                { error ->
-                    // Handle the error
-                    onError("Error: ${error.message}")
-                })
-
-            queue.add(request)
-        }
+    fun appendDrivingSession(drivingSession: Driving) {
+        drivingSessions += drivingSession
+        notifyItemInserted(drivingSessions.lastIndex)
     }
 }
